e8953ef7b2d524d6b25413cc4259611e03e282c7
Pantelis Orfanidis <Pantelis Orfanidis>
2024-10-03 00:29:02 +0300

    Fine tune layout

--- src/main.py
+++ src/main.py
@@ -1,8 +1,8 @@
 import pandas as pd
-from preprocessing import primary_preprocess
+from utils import primary_preprocess
 
 chunk_size = 10
-chunks = pd.read_csv('data/Greek_Parliament_Proceedings_1989_2020.csv', chunksize=chunk_size, encoding='utf-8')
+chunks = pd.read_csv('data/dataset.csv', chunksize=chunk_size, encoding='utf-8')
 
 for chunk in chunks:
     chunk['processed_speech'] = chunk['speech'].apply(primary_preprocess)
